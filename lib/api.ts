d465d0aaeb4ecb70b855fc63640a053d326096fd
Dinsha Gupta <vidit.gupta2006@gmail.com>
2026-05-28 12:22:43 +0530

    deployed with url

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://github.com/ViditGupta0603/auctra-backend/api",
 });
 
 /**
--- lib/socket.ts
+++ lib/socket.ts
@@ -1,6 +1,6 @@
 // Use require to avoid TS module resolution issues when types are missing
 const { io } = require("socket.io-client");
 
-const socket = io("http://localhost:5000");
+const socket = io("https://github.com/ViditGupta0603/auctra-backend");
 
 export default socket;
